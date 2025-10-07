58a6d2c6b7bc24c52d9a05727d46fa375af2ac44
onyourrndra <raendra.bastian7@smk.belajar.id>
2025-10-07 14:48:41 +0800

    update (lagi)

--- FormJurusan.Designer.cs
+++ FormJurusan.Designer.cs
@@ -30,40 +30,42 @@
         {
             this.txbKodeJurusan = new System.Windows.Forms.TextBox();
             this.txbJurusan = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblNamaJurusan = new System.Windows.Forms.Label();
             this.btnSimpan = new System.Windows.Forms.Button();
             this.btnBatal = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblKodeJurusan = new System.Windows.Forms.Label();
+            this.dgvJurusan = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJurusan)).BeginInit();
             this.SuspendLayout();
             // 
             // txbKodeJurusan
             // 
-            this.txbKodeJurusan.Location = new System.Drawing.Point(269, 49);
+            this.txbKodeJurusan.Location = new System.Drawing.Point(142, 21);
             this.txbKodeJurusan.Name = "txbKodeJurusan";
-            this.txbKodeJurusan.Size = new System.Drawing.Size(163, 20);
+            this.txbKodeJurusan.Size = new System.Drawing.Size(117, 20);
             this.txbKodeJurusan.TabIndex = 0;
             this.txbKodeJurusan.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txbJurusan
             // 
-            this.txbJurusan.Location = new System.Drawing.Point(32, 49);
+            this.txbJurusan.Location = new System.Drawing.Point(142, 52);
             this.txbJurusan.Name = "txbJurusan";
-            this.txbJurusan.Size = new System.Drawing.Size(117, 20);
+            this.txbJurusan.Size = new System.Drawing.Size(174, 20);
             this.txbJurusan.TabIndex = 1;
             // 
-            // label1
+            // lblNamaJurusan
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(205, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(10, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = ":";
-            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            this.lblNamaJurusan.AutoSize = true;
+            this.lblNamaJurusan.Location = new System.Drawing.Point(32, 52);
+            this.lblNamaJurusan.Name = "lblNamaJurusan";
+            this.lblNamaJurusan.Size = new System.Drawing.Size(44, 13);
+            this.lblNamaJurusan.TabIndex = 2;
+            this.lblNamaJurusan.Text = "Jurusan";
+            this.lblNamaJurusan.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // btnSimpan
             // 
-            this.btnSimpan.Location = new System.Drawing.Point(42, 116);
+            this.btnSimpan.Location = new System.Drawing.Point(29, 93);
             this.btnSimpan.Name = "btnSimpan";
             this.btnSimpan.Size = new System.Drawing.Size(75, 23);
             this.btnSimpan.TabIndex = 3;
@@ -72,36 +74,46 @@
             // 
             // btnBatal
             // 
-            this.btnBatal.Location = new System.Drawing.Point(42, 145);
+            this.btnBatal.Location = new System.Drawing.Point(29, 122);
             this.btnBatal.Name = "btnBatal";
             this.btnBatal.Size = new System.Drawing.Size(75, 23);
             this.btnBatal.TabIndex = 4;
             this.btnBatal.Text = "Batal";
             this.btnBatal.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // lblKodeJurusan
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 21);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Kode Jurusan";
+            this.lblKodeJurusan.AutoSize = true;
+            this.lblKodeJurusan.Location = new System.Drawing.Point(32, 21);
+            this.lblKodeJurusan.Name = "lblKodeJurusan";
+            this.lblKodeJurusan.Size = new System.Drawing.Size(72, 13);
+            this.lblKodeJurusan.TabIndex = 5;
+            this.lblKodeJurusan.Text = "Kode Jurusan";
+            // 
+            // dgvJurusan
+            // 
+            this.dgvJurusan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJurusan.Location = new System.Drawing.Point(142, 105);
+            this.dgvJurusan.Name = "dgvJurusan";
+            this.dgvJurusan.Size = new System.Drawing.Size(483, 162);
+            this.dgvJurusan.TabIndex = 6;
             // 
             // FormJurusan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(662, 332);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvJurusan);
+            this.Controls.Add(this.lblKodeJurusan);
             this.Controls.Add(this.btnBatal);
             this.Controls.Add(this.btnSimpan);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNamaJurusan);
             this.Controls.Add(this.txbJurusan);
             this.Controls.Add(this.txbKodeJurusan);
             this.Name = "FormJurusan";
             this.Text = "FormJurusan";
             this.Load += new System.EventHandler(this.FormJurusan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJurusan)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,9 +123,10 @@
 
         private System.Windows.Forms.TextBox txbKodeJurusan;
         private System.Windows.Forms.TextBox txbJurusan;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNamaJurusan;
         private System.Windows.Forms.Button btnSimpan;
         private System.Windows.Forms.Button btnBatal;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblKodeJurusan;
+        private System.Windows.Forms.DataGridView dgvJurusan;
     }
 }
