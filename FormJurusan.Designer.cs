17b29da4ad7a6251e33ccd600dc1d3af663a9f7f
onyourrndra <raendra.bastian7@smk.belajar.id>
2025-10-07 13:58:22 +0800

    jurusan

---
+++ FormJurusan.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace LatihanGithub
+{
+    partial class FormJurusan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormJurusan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 332);
+            this.Name = "FormJurusan";
+            this.Text = "FormJurusan";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
