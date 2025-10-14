9fd12c2c8ea8811071f0b458f3801a24c52b1472
FebiRpl <febprogrammer@gmail.com>
2025-10-14 15:32:57 +0700

    Test

--- FormLogin.Designer.cs
+++ FormLogin.Designer.cs
@@ -92,12 +92,14 @@
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Tomato;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button2.Location = new System.Drawing.Point(287, 180);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 6;
             this.button2.Text = "Exit";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
